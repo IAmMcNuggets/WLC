896699ad45c2512456b32669cfc19843e627c9c6
IAmMcNuggets <dtweisberg@gmail.com>
2024-10-27 10:36:54 -0400

    Update Training.tsx

--- WLC2/my-app/src/Pages/Training.tsx
+++ WLC2/my-app/src/Pages/Training.tsx
@@ -1,27 +1,48 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const TrainingPage = styled.div`
+  min-height: 100vh;
+  padding: 20px;
+  box-sizing: border-box;
+  background-image: url(${require('../Background/86343.jpg')});
+  background-size: cover;
+  background-position: center;
+  background-attachment: fixed;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const TrainingTitle = styled.h1`
+  text-align: center;
+  width: 100%;
+  margin-top: 20px;
+  margin-bottom: 30px;
+  color: black;
+  text-shadow: 2px 2px 4px rgba(0,0,0,0.5);
+  font-size: 2.5rem;
+`;
+
 const TrainingContainer = styled.div`
   padding: 20px;
   background-color: rgba(255, 255, 255, 0.9);
   border-radius: 10px;
   margin: 20px;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-`;
-
-const TrainingTitle = styled.h1`
-  font-size: 2rem;
-  color: #333;
-  margin-bottom: 20px;
+  width: 90%;
+  max-width: 600px;
 `;
 
 const Training: React.FC = () => {
   return (
-    <TrainingContainer>
+    <TrainingPage>
       <TrainingTitle>Training</TrainingTitle>
-      {/* Add your training content here */}
-      <p>Welcome to the Training page. Content coming soon!</p>
-    </TrainingContainer>
+      <TrainingContainer>
+        {/* Add your training content here */}
+        <p>Welcome to the Training page. Content coming soon!</p>
+      </TrainingContainer>
+    </TrainingPage>
   );
 };
 
